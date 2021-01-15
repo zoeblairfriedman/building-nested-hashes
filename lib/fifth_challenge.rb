5ed250016375792458897ab68861468b5d9ae518
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-01-14 19:47:18 -0500

    commit tree f2219b59cf64cb2393761397a3efd581e62a911f

--- lib/fifth_challenge.rb
+++ lib/fifth_challenge.rb
@@ -24,8 +24,8 @@ def fifth_challenge
   }
 
   #code your solution here:
-
-
+epic_tragedy[:montague][:hero][:status] = "dead"
+epic_tragedy[:capulet][:heroine][:status] = "dead"
   #Don't touch the following line! The method must return our newly modified epic tragedy hash
   epic_tragedy
 end
